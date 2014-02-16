02bc0022b4eb4ff07c38c17ecd9aa86dea48b243
sornars <sornars@users.noreply.github.com>
2014-02-16 18:20:53 +0000

    Updated package with README.md and LICENSE.

---
+++ README.md
@@ -0,0 +1,5 @@
+Massmodify Module
+========================
+
+This simple project is an example repo for Python projects. The module can be
+used to modify all files in a directory.
--- setup.py
+++ setup.py
@@ -1,9 +1,17 @@
 from distutils.core import setup
 
+with open('README.md') as f:
+    readme = f.read()
+
+with open('LICENSE') as f:
+    license = f.read()
+
 setup(name='massmodify',
       version='0.1.0',
       description='Modify multiple files in a folder.',
       author='Shivan Sornaraja',
       author_email='ssornarajah@mediaplex.com',
-      py_modules=['massmodify']
+      py_modules=['massmodify'],
+      long_description=readme,
+      license=license
       )
