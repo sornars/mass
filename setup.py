32ed49c4443afeedce7cae0b9f7ad78a686967b1
sornars <sornars@users.noreply.github.com>
2014-02-16 18:13:59 +0000

    Made massmodify a disutils package.

---
+++ setup.py
@@ -0,0 +1,9 @@
+from distutils.core import setup
+
+setup(name='massmodify',
+      version='0.1.0',
+      description='Modify multiple files in a folder.',
+      author='Shivan Sornaraja',
+      author_email='ssornarajah@mediaplex.com',
+      py_modules=['massmodify']
+      )
